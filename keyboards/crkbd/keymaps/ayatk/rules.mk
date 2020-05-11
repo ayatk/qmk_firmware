cefbd4b2471c798371ff72ea501febe7493ecab3
ayatk <git@ayatk.com>
2020-05-11 10:07:08 +0900

    delete combo

--- keyboards/crkbd/keymaps/ayatk/rules.mk
+++ keyboards/crkbd/keymaps/ayatk/rules.mk
@@ -1,5 +1,4 @@
 LOCAL_GLCDFONT = yes         # use each keymaps "crkbdfont.h" insted of "lib/glcdfont.c"
-COMBO_ENABLE = yes
 
 ifeq ($(strip $(LOCAL_GLCDFONT)), yes)
     OPT_DEFS += -DLOCAL_GLCDFONT
